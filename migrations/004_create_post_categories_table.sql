e7e52a1e7621c30362d70e2c3349084f85a27558
AllmayJapan <nobunaga.oda.japan@gmail.com>
2025-01-08 11:20:35 +0900

    define verified

--- migrations/004_create_post_categories_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE post_categories (
-	post_id INT NOT NULL,
-	category_id INT NOT NULL,
-	PRIMARY KEY (post_id, category_id),
-	FOREIGN KEY (post_id) REFERENCES posts(post_id),
-	FOREIGN KEY (category_id) REFERENCES categories(category_id)
-)
